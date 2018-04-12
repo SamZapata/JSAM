091c368205012e9fa5db0445303f9b3a091c19c3
SamZapata <legionario.zapata@gmail.com>
2018-04-11 21:25:50 -0500

    remove module 'bootstrap4' to working on pythonanywhere

--- jsamzapata/settings.py
+++ jsamzapata/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'blog',
-    'bootstrap4',
+#    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
